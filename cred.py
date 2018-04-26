138b2464d8a5ef5582e67a4f7fcc2295663ad68c
Jen Yu <jyu7@stuy.edu>
2018-04-26 02:00:19 -0400

    ugh

--- cred.py
+++ cred.py
@@ -1,3 +1,10 @@
+'''
+Jen Yu
+Softdev pd7
+2018-04-26
+K#15: Do You Even List?
+'''
+
 UC = "QWERTYUIOPASDFGHJKLZXCVBNM"
 N = "1234567890"
 A = ".?!&#,;:-_*"
@@ -15,12 +22,41 @@ def a_count(p):
     return len(vals)
 
 def pass_check(p):
-    if (uc_count(p) >= 1) and (n_count(p) > 0) and (a_count(p) > 0):
-        return "good password"
-    #print uc_count(p)
-    #print n_count(p)
-    return "bad password"
+	u = uc_count(p)
+	n = n_count(p)
+	a = a_count(p)
+	score = 10
+	r = " \n"
+	if len(p) < 6: 
+		score -= 5
+		r += "Less than 6 characters: too short!\n"
+		if n < 2 or a < 2: 
+			score -= 3
+			r += "Not enough numbers or alphanumerical characters!\n"
+			if u > 2:
+				return "Score: " + str(score) + r
+			else:
+				return "Score: " + str(score) + r + "Not enough capitalization!\n"
+		else: 
+			if u < 2:
+				return "Score: " + str(score) + r + "Not enough capitalization!\n"
+			return "Score: " + str(score) + r 
+	else:
+		if u < len(p)/2 - n - a:
+			score -= 3
+			r += "Need a healthy mixture of capitalization.\n"
+			if n < 2 or a < 2: 
+				score -= 2
+				return "Score: " + str(score) + r + "Not enough numbers or alphanumerical characters!\n"
+			else: 
+				return "Score: " + str(score) + r
+		else:
+			if n < 2 or a < 2: 
+				score -= 2
+				return "Score: " + str(score) + r + "Not enough numbers or alphanumerical characters!\n"
+			else: 
+				return "Score: " + str(score) + r
 
-print pass_check("hello")
+print pass_check("hello") 
 print pass_check("1Wwerwer!")
 print pass_check("!1WeRe")
